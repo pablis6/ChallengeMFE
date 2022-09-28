035663392f6327b229948c94a15c65ccc867404e
Pablo Fernandez Grado <pablo93fernandez@gmail.com>
2022-09-28 12:44:22 +0200

    configuracion module federation

--- apps/sopra-chat/module-federation.config.js
+++ apps/sopra-chat/module-federation.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   name: 'sopra-chat',
-  remotes: [],
+  remotes: ['login-form'],
 };
--- apps/sopra-chat/webpack.config.js
+++ apps/sopra-chat/webpack.config.js
@@ -1,3 +1,13 @@
-const { withModuleFederation } = require('@nrwl/angular/module-federation');
-const config = require('./module-federation.config');
-module.exports = withModuleFederation(config);
+const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+
+module.exports = withModuleFederationPlugin({
+
+  remotes: {
+    "loginForm": "http://localhost:4201/remoteEntry.js",    
+  },
+
+  shared: {
+    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+  },
+
+});
